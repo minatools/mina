62adba0c54274857e3d7f8576429d444b15c56d7
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2020-12-13 23:09:10 -0600

    update bzl/tools/syslibs.sh

--- bzl/tools/syslibs.sh
+++ bzl/tools/syslibs.sh
@@ -1,12 +1,53 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get install --yes \
-     pkg-config  \
-     libgmp-dev  \ ## required by zarith (libgmp3-dev?)
-     libffi-dev  \ ## required by ctypes-foreign, used by orocksdb
-     libssl-dev  \ ## required by async_ssl
-     libpq-dev     ## required by postgresql, caqti-driver-postgresql
+## Install libs needed by OPAM packages.
+
+ZARITH=
+CTYPES=
+ASYNC_SSL=
+POSTGRESQL=
+POSTGRESQL_C=
+
+UNAME=$( command -v uname)
+
+case $( "${UNAME}" | tr '[:upper:]' '[:lower:]') in
+    linux*)
+        printf 'linux\n'
+        ZARITH=libgmp3-dev
+        CTYPES=libffi-dev
+        ASYNC_SSL=libssl-dev
+        POSTGRESQL_C=libpq-dev
+        ;;
+    darwin*)
+        printf 'darwin\n'
+        export HOMEBREW_NO_AUTO_UPDATE=1
+        brew update-reset
+        ZARITH=gmp
+        CTYPES=libffi
+        ASYNC_SSL=openssl@1.1
+        POSTGRESQL=postgresql
+        POSTGRESQL_C=libpq
+        ;;
+    # msys*|cygwin*|mingw*)
+    #   # or possible 'bash on windows'
+    #   printf 'windows\n'
+    #   ;;
+    # nt|win*)
+    #   printf 'windows\n'
+    #   ;;
+    *)
+        printf 'unknown os\n'
+        ;;
+esac
+
+## FIXME: also support homebrew for MacOS
+# sudo apt-get update
+# sudo apt-get install --yes \
+#      pkg-config  \
+#      ZARITH \
+#      CTYPES \
+#      ASYNC_SSL \
+#      POSTGRESQL
 
 
 # OPAM:
